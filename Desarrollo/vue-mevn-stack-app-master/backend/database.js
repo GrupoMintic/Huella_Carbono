b4dfcbb40e7eed0ca1d1627edc44ab40ae630de0
FelipeXD1406 <86898423+FelipeXD1406@users.noreply.github.com>
2021-09-22 19:25:02 -0500

    desarrollo

--- Desarrollo/vue-mevn-stack-app-master/backend/database.js
+++
@@ -1,9 +0,0 @@
-// module.exports = {
-//   db: "mongodb://localhost:27017/vuecrudmevn",
-// };
-
-module.exports = {
-  db:
-  "mongodb+srv://lmolero:1126254560@cluster0.bpms5.mongodb.net/vuecrudmevn?retryWrites=true&w=majority"
- };
- 
